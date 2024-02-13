d965466217e550c9283076ea1973f9860bf3b5c4
nathan2lim <gilbertnathan123@icloud.com>
2024-02-13 10:47:10 +0100

    affichage des vanne dans la page principale sans filtreiclo

--- EntretienVannes/EntretienVannes/urls.py
+++ EntretienVannes/EntretienVannes/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appliVanne import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("vannes/", views.listeVanne),
 ]
--- EntretienVannes/appliVanne/views.py
+++ EntretienVannes/appliVanne/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from appliVanne.models import Vanne
+from appliVanne.models import Vanne, ACTIONNEUR, CORPS, POSITIONNEUR, TYPEPOSITIONNEUR, FOURNISSEUR
 
 # Create your views here.
+
+
+#affichage des vannes 
+def vannes(request) :
+    LesVannes = Vanne.objects.all() 
+    return render(request, "appliVanne/vannes.html", {"vannes": LesVannes})
+
+def listeVanne (request):
+    lesVannes  = Vanne.objects.all()
+    return render(request, "appliVanne/vannes.html", {"listeVannes": lesVannes})
+
+
+    
+    
+    
+    
